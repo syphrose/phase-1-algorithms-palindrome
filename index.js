a81afa8ae0c93872880ea313abd98fce6e2368d1
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-19 00:29:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let wordLowerCase=word.toLowerCase();
+  let wordSeparate=wordLowerCase.split('').reverse().join('')
+
+   if(wordLowerCase===wordSeparate){
+     return true;
+   }else{
+     return false;
+   }
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
